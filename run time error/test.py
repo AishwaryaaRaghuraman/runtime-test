71dabb800612f86faf425481cca49ac6caf47292
AishwaryaaRaghuraman <143588049+AishwaryaaRaghuraman@users.noreply.github.com>
2023-12-15 15:31:54 +0530

    Update test.py

--- run time error/test.py
+++ run time error/test.py
@@ -1,23 +1,14 @@
 def divide_numbers(numerator, denominator):
-    try:
-        # Attempt to perform the division
-        result = numerator / denominator
-        return result
-    except ZeroDivisionError:
-        # Handle the error by returning a descriptive error message
-        return "Error: Division by zero is undefined."
-
+    # This line may cause a ZeroDivisionError
+    result = numerator / denominator
+    return result
+ 
 # Example usage:
 numerator_value = 10
-denominator_value = 2
-
-# Attempt to divide the numbers and handle any errors
+denominator_value = 0
+ 
+# The following line will raise a ZeroDivisionError
 result = divide_numbers(numerator_value, denominator_value)
-
-# Check if the result is an error message
-if isinstance(result, str):
-    # Print the error message
-    print(result)
-else:
-    # Print the result of the division
-    print("Result:", result)
+ 
+# This line will not be executed due to the previous error
+print("Result:", result)
