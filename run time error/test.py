0d9f822db54c067b1d7ed40c73dce9b2b450cf6a
AishwaryaaRaghuraman <143588049+AishwaryaaRaghuraman@users.noreply.github.com>
2023-12-18 18:34:58 +0530

    Update test.py

--- run time error/test.py
+++ run time error/test.py
@@ -1,14 +1,7 @@
-def divide_numbers(numerator, denominator):
-    # This line may cause a ZeroDivisionError
+def divide_numbers(numerator, denominator)
     result = numerator / denominator
     return result
- 
-# Example usage:
 numerator_value = 10
-denominator_value = 0
- 
-# The following line will raise a ZeroDivisionError
+denominator_value = 0 
 result = divide_numbers(numerator_value, denominator_value)
- 
-# This line will not be executed due to the previous error
 print("Result:", result)
